8b9159290e6f927dc3ac5d9d98268cf1199afdc6
StuartGrimes <stuartrgrimes@gmail.com>
2016-08-04 14:09:08 +0100

    some structure in place - Shane to insert bootstrap theme

--- app_server/controllers/session.js
+++ app_server/controllers/session.js
@@ -1,4 +1,4 @@
 //working out session from form inputs...
 module.exports.sessionspec = function(req, res) {
-    console.log('Simple output...')
+    res.render('sessionsettings', {title: 'Swim Generator'});
 }
